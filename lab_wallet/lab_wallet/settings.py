70c93a360a15f091b9ef427a83745d6874566ea9
SatoTen <dsnokasetto@gmail.com>
2024-03-27 16:34:50 +0900

    create home directry

--- lab_wallet/lab_wallet/settings.py
+++ lab_wallet/lab_wallet/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'lab_wallet',
-    '',
+    'manager.apps.ManagerConfig',
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
